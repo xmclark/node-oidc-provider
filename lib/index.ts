0853c46ac5a7c5843d1ed27120e3b22a77b6495c
Mackenzie Clark <mackenzie.a.z.c@gmail.com>
2020-02-01 09:30:34 -0800

    port index.js to index.ts

---
+++ lib/index.ts
@@ -0,0 +1,14 @@
+export Provider from './provider';
+export errors from './helpers/errors';
+export { DYNAMIC_SCOPE_LABEL } from './consts';
+export interaction_policy from './helpers/interaction_policy';
+
+// OLD
+// const Provider = require('./provider');
+// const errors = require('./helpers/errors');
+// const { DYNAMIC_SCOPE_LABEL } = require('./consts');
+// module.exports = Provider;
+// module.exports.Provider = Provider;
+// module.exports.errors = errors;
+// module.exports.DYNAMIC_SCOPE_LABEL = DYNAMIC_SCOPE_LABEL;
+// module.exports.interactionPolicy = require('./helpers/interaction_policy');
